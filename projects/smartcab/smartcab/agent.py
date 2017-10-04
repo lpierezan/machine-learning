3185ad99ff5d362d25d65e359de97389219db04f
Lucas Pierezan Magalhães <lucas.pierezan@gmail.com>
2017-10-04 08:07:53 -0300

    Minor fix in Agent.py
    Testing "if learning" in createQ()

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -99,7 +99,7 @@ class LearningAgent(Agent):
         # When learning, check if the 'state' is not in the Q-table
         # If it is not, create a new dictionary for that state
         #   Then, for each action available, set the initial Q-value to 0.0
-        if(state not in self.Q):
+        if((self.learning) and (state not in self.Q)):
             self.Q[state] = {action:0.0 for action in self.valid_actions}
             
         return
